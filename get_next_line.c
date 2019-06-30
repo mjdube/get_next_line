7a4e30713bcbd9a16cb601a8e0f89b4a1af9ca0a
Mlungisi DUBE <mdube@c4r13s7.wethinkcode.co.za>
2019-06-30 15:48:17 +0200

    30th June 2019

--- get_next_line.c
+++ get_next_line.c
@@ -6,69 +6,63 @@
 /*   By: mdube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 10:02:55 by mdube             #+#    #+#             */
-/*   Updated: 2019/06/29 07:53:32 by mdube            ###   ########.fr       */
+/*   Updated: 2019/06/30 15:42:39 by mdube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <string.h>
 
-static int 		checking(char **line, char **save, char *temp)
+static int			checknewline(char *buf)
 {
-	int endl;
+	int		i;
 
-	save[fd] = ft_strsub(save[fd], (ft_strlen(*line) + endl), (ft_strlen(save[fd]) - (ft_strlen(*line) + endl)));
-	ft_strdel(&tmp);
-	if (!(!save[fd] && !ft_strlen(*line)))
-		return (1);
-	else
-		return (0);
+	i = 0;
+	while (buf[i])
+	{
+		if (buf[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-int			get_next_line(const int fd, char **line)
+static char		*afternewline(const char *buf)
 {
-	char			buf[BUFFER_SIZE + 1];
-	static char		*save[1024];
+	size_t		i;
+	char	*fresh;
+
+	i = 0;
+	while (buf[i])
+	{
+		if (buf[i] == '\n')
+			break;
+		i++;
+	}
+	fresh = ft_strnew(i);
+	fresh = ft_strncpy(fresh, buf, i);
+	return (fresh);
+}
+
+int					get_next_line(const int fd, char **line)
+{
+	static char		*s[4096];
 	int				ret;
-	char			*temp;
-	
-	if (fd < 0 || !(*line) || !(save[fd] = ft_strnew(1)))
+	char			buf[BUFFER_SIZE + 1];
+
+	if (fd < 0 || !*(line))
 		return (-1);
-	while ((ret = read(fd, buf, BUFFER_SIZE) > 0) && !ft_strchr(buf, '\n'))
+	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0 && (checknewline(buf) == 0))
 	{
 		buf[ret] = '\0';
-
-		/*
-		if (ft_strchr(buf, '\n'))
-		{
-			*line = (stuff up to not incl '\n');
-			return (1);
-			save = ft_strsub(buff, (ft_strlen(buff) - ft_strlen(temp), ft_strlen(temp));
-			temp = ft_strnew(ft_strlen(buf));
-		}
-		*/
-		temp = save[fd];
-		save[fd] = ft_strjoin(save[fd], buf);
-		ft_strdel(&temp);
+		s[fd] = buf;
 	}
-	if (ret == -1 || !(*temp == save[fd]))
-	{
-		if (ret == -1)
-			return (-1);
-		return (0);
-	}
-	if (ft_strchr(buf, '\n'))
-		*line = lseek(fd, ret, SEEK_CUR);
+	if (ret == -1)
+		return (-1);
+	if (checknewline(buf) == 1)
+		*line = ft_strsub(s[fd], 0, ft_strchr(s[fd], '\n') - s[fd]);
 	else
-		*line = ft_strdup(save[fd]);
-	return (checking(line, save, temp))
+		*line = ft_strdup(s[fd]);
+	s[fd] = afternewline(*line);
+	return (!(!s[fd] && !ft_strlen(*line)));
 }
-/*
-if ((endl = (ft_strchr(c[fd], '\n') > 0)))
-		*line = ft_strsub(c[fd], 0, ft_strchr(c[fd], '\n') - c[fd]);
-	else
-		*line = ft_strdup(c[fd]);
-	c[fd] = ft_strsub(c[fd], (unsigned int)(ft_strlen(*line) + endl),
-			(size_t)(ft_strlen(c[fd]) - (ft_strlen(*line) + endl)));
-	ft_strdel(&tmp);
-	return (!(!c[fd] && !ft_strlen(*line)));*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,18 @@
 /*   By: mdube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 13:17:58 by mdube             #+#    #+#             */
-/*   Updated: 2019/06/27 17:35:28 by mdube            ###   ########.fr       */
+/*   Updated: 2019/06/30 15:11:40 by mdube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFFER_SIZE 32
-
-#include <string.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <string.h>
+# include "libft/libft.h"
 
 int			get_next_line(const int fd, char **line);
 
